1fee87d903108c1e3c744d1b95536e21a09e9511
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-15 23:57:50 +0100

    use prefetch instead of query the repo

--- compile-all-examples.sh
+++ compile-all-examples.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
+#example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
+example_repo=$(nix flake prefetch github:esp0xdeadbeef/network-labs --json | jq -r .storePath)
 
 find $example_repo -name 'intent.nix' -print0 |
   while IFS= read -r -d '' f; do

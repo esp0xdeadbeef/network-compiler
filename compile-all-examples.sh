1d051f7ce8479aa2061c10032a57c66d2468313a
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-04-11 17:29:45 +0200

    bump and update .sh scripts to use locked network-labs flake input store path

--- compile-all-examples.sh
+++ compile-all-examples.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
 example_repo=$(nix flake prefetch github:esp0xdeadbeef/network-labs --json | jq -r .storePath)
+example_repo="$(
+  nix flake archive --json "path:$ROOT" \
+    | jq -er '.inputs["network-labs"].path'
+)"
+
 
 find $example_repo -name 'intent.nix' -print0 |
   while IFS= read -r -d '' f; do
--- compile-one-example.sh
+++ compile-one-example.sh
@@ -2,6 +2,10 @@
 #nix run path:.#compile -- git@github.com:esp0xdeadbeef/network-labs.git/examples/single-wan/intent.nix
 #example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
 example_repo=$(nix flake prefetch github:esp0xdeadbeef/network-labs --json | jq -r .storePath)
+example_repo="$(
+  nix flake archive --json "path:$ROOT" \
+    | jq -er '.inputs["network-labs"].path'
+)"
 #example_repo=$(echo /home/deadbeef/github/network-labs)
 #nix run .#compile -- $example_repo/examples/multi-wan/intent.nix
 nix run .#compile -- $example_repo/examples/multi-wan/intent.nix
